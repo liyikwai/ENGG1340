94c03c6b290fccc13f306f9e44f7d2e4ac1d2767
Vict0rW0ngHKU <47235424+Vict0rW0ngHKU@users.noreply.github.com>
2019-04-23 18:02:28 +0800

    Update Adding_Records.cpp

--- Adding_Records.cpp
+++ Adding_Records.cpp
@@ -1,107 +1,193 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <map>
-#include <algorithm>
 using namespace std;
 
-struct date{
+struct record {
+  int Type; // Income or Expense, 2 for Income, 1 for Expense
+  int Amount;
+  string Info;
+};
+struct date {
   int Day;
   int Month;
   int Year;
+  record rec;
 };
-
-struct account{
-  date Date;
+bool operator<(const record &a, const record &b) {
+  if (a.Type < b.Type)
+    return true;
+  if (a.Type > b.Type)
+    return false;
+  if (a.Amount < b.Amount)
+    return true;
+  if (a.Amount > b.Amount)
+    return false;
+  return a.Info < b.Info;
+}
+bool operator<(const date &a, const date &b) {
+  if (a.Year < b.Year)
+    return true;
+  if (a.Year > b.Year)
+    return false;
+  if (a.Month < b.Month)
+    return true;
+  if (a.Month > b.Month)
+    return false;
+  if (a.Day < b.Day)
+    return true;
+  if (a.Day > b.Day)
+    return false;
+  return a.rec < b.rec;
+}
+void Increase_Size(date *&Date, int &size) {
+  date *temp = new date [size + 10];
+  for (int i = 0; i < size; i++)
+    temp[i] = Date[i];
+  delete[] Date;
+  Date = temp;
+  size += 10;
+}
+void Append_Income(date *&Date, int count) {
+  int DD, MM, YYYY;
   double Income;
-  string Income_Info;
+  string Info;
+  cout << "DD MM YYYY: " << endl;
+  cin >> DD >> MM >> YYYY;
+  Date[count].Day = DD;
+  Date[count].Month = MM;
+  Date[count].Year = YYYY;
+  cout << "Income: " << endl;
+  cin >> Income;
+  cout << "Info: " << endl;
+  cout << "E: Earned Income\tF: Portfolio Income\tP: Passive Income" << endl;
+  cin >> Info;
+  Date[count].rec.Amount = Income;
+  Date[count].rec.Type = 2;
+  Date[count].rec.Info = Info;
+}
+void Append_Expense(date *&Date, int count){
+  int DD, MM, YYYY;
   double Expense;
-  string Expense_Info;
-  account();
-};
-
-account::account(){
-  Income_Info = "NULL";
-  Expense_Info = "NULL";
+  string Info;
+  cout << "DD MM YYYY: " << endl;
+  cin >> DD >> MM >> YYYY;
+  Date[count].Day = DD;
+  Date[count].Month = MM;
+  Date[count].Year = YYYY;
+  cout << "Expense: " << endl;
+  cin >> Expense;
+  cout << "Info: " << endl;
+  cout << "T: Transportation\tF: Food & Drinks\tL: Living & Others" << endl;
+  cin >> Info;
+  Date[count].rec.Amount = Expense;
+  Date[count].rec.Type = 1;
+  Date[count].rec.Info = Info;
 }
-
-void increase_size(account *&user, int &account_size){
-  account *temp = new account[account_size+1];
-  for (int i = 0; i < account_size; i++){
-    temp[i] = user[i];
-  }
-  delete []user;
-  user = temp;
-  account_size += 1;
-}
-
-void Append_Records(char add, account *user, int tail){
-  int amount, d, m, y;
-  string remark;
-  cout << "Please enter the date: Day Motnth Year " << endl;
-  cin >> d >> m >> y;
-  cout << "Please enter the amount " << endl;
-  cin >> amount;
-  cout << "Please enter the remark of this record " << endl;
-  cin >> remark;
-  user[tail].Date.Day = d;
-  user[tail].Date.Month = m;
-  user[tail].Date.Year = y;
-  if (add == 'I'){
-    user[tail].Income = amount;
-    user[tail].Income_Info = remark;
-  }
-  if (add == 'E'){
-    user[tail].Expense = amount;
-    user[tail].Expense_Info = remark;
-  }
-}
-
-void Create_record(account *user, int tail){
-  ofstream fout;
-  fout.open("Financial_Record.txt");
-  if (fout.fail()){
-    exit(1);
-  }
-  for(int i = 0; i < tail; i++){
-    fout << "Date: " << user[i].Date.Day << "/" << user[i].Date.Month << "/" << user[i].Date.Year << endl;
-    fout << "    Income: " << user[i].Income << endl;
-    fout << "    Income remark: " << user[i].Income_Info << endl;
-    fout << "    Expenseense " << user[i].Expense << endl;
-    fout << "    Expenseense remark: "<< user[i].Expense_Info << endl;
-  }
-  fout.close();
-}
-
-int main(){
-  char add;
-  double record;
-  int tail = 0;
-  int account_size = 10;
-  account *user = new account[account_size];
-  cout << "----------------------------" << endl;
-  cout << "|ENGG1340 Accounting System|" << endl;
-  cout << "|          Group73         |" << endl;
-  cout << "----------------------------" << endl;
-  cout << endl;
-  while(1){
-    cout << "Please enter: " << endl;
-    cout << "I: Adding Income " << endl;
-    cout << "E: Adding Expenseense " << endl;
-    cout << "Q: Quit " << endl;
-    cin >> add;
-    if (add == 'Q'){
+void Sort(date *&Date, int count){
+  int Actual_Count = count;
+  for (int i = 0; i < count; i++){
+    if (Date[i].rec.Type == 0){
+      Actual_Count = i + 1;
       break;
     }
-    else{
-      Append_Records(add, user, tail);
-      tail++;
-      if (tail == account_size){
-        increase_size(user, account_size);
+  }
+  for (int i = 0; i < Actual_Count - 1; i++)
+    for (int j = i + 1; j < Actual_Count; j++)
+      if (Date[i] < Date[j]){
+        date *Temp = new date;
+        *Temp = Date[i];
+        Date[i] = Date[j];
+        Date[j] = *Temp;
+        delete Temp;
+      }
+}
+
+int main() {
+  string Command;
+  int count = 0, size = 100;
+  date *Date = new date[100];
+  double Budget = -1;
+  double Monthly_Balance = 0, Monthly_Income = 0, Monthly_Expense = 0;
+  while(true) {
+    cout << "Please enter the following commands:" << endl;
+    cout << "I: Adding income" << endl; // Done
+    cout << "E: Adding Expense" << endl; // Done
+    cout << "P: Present Information" << endl; // Done
+    cout << "C: Change Records" << endl;
+    cout << "D: Delete Records" << endl;
+    cout << "S: Search Records" << endl;
+    cout << "R: Report" << endl; 
+    cout << "B: Budget Setting" << endl; // Need Monthly_Income
+    cout << "G: Goal Setting" << endl;   // Need Monthly_Balance
+    cout << "Q: Quit" << endl;  //Done
+    cin >> Command;
+    if (Command == "Q")
+      break;
+    if (Command == "I") {
+      if (count == size)
+        Increase_Size(Date, size);
+      Append_Income(Date, count);
+      count++;
+      continue;
+    }
+    else if (Command == "E"){
+      if (count == size)
+        Increase_Size(Date, size);
+      Append_Expense(Date, count);
+      count++;
+      continue;
+    }
+    //C, D
+    Sort(Date, count);
+    // monthly income, monthly expense, monthly balance, monthly spending compared to budget
+    if (Command == "P"){
+      for (int i = 0; i < count; i++){
+        if (Date[i].rec.Type == 0)
+          break;
+        cout << Date[i].Day << " " << Date[i].Month << " " << Date[i].Year << " ";
+        if (Date[i].rec.Type == 2)
+          cout << "Income" << " ";
+        if (Date[i].rec.Type == 1)
+          cout << "Expense" << " ";
+        cout << Date[i].rec.Amount << " " << Date[i].rec.Info << endl;
+      }
+      continue;
+    }
+    if (Command == "B"){
+      while(true){
+        cout << "Set your budget: " << endl;
+        cin >> Budget;
+        if (Budget <= Monthly_Income){
+          cout << "OK." << endl;
+          break;
+        }
+        else
+          cout << "Beyond Your financial capability! Reset your budget!" << endl;
       }
     }
+    if (Command == "G"){
+      double Goal, Monthly_Goal;
+      cout << "What's your goal?" << endl;
+      cin >> Goal;
+      cout << "That will take you at least " << Goal / Monthly_Balance << endl;
+      cout << "How much per month do you want to save for it?" << endl;
+      while(true){
+        if (Monthly_Balance < 0) {
+          cout << "You are bleeding money, change your spending habit first!" << endl;
+          break;
+        }
+        cin >> Monthly_Goal;
+        if(Monthly_Goal > Monthly_Balance)
+          cout << "That's beyond your financial capability!";
+        else
+          cout << "That will take you " << Goal / Monthly_Goal << "to achieve!" << endl;
+      }
+    }
+    // S, R
+    
   }
-  Create_record(user, tail);
-  delete []user;
+  delete[] Date;
   return 0;
 }
+// Also need to do file I/O
+// Split .cpp and create makefile
