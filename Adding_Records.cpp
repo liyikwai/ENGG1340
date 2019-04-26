ba7a12a68b5b3794f38fa10158c937d6bc1bebff
LI YIK WAI <47166479+liyikwai@users.noreply.github.com>
2019-04-26 19:54:20 +0800

    Update Adding_Records.cpp

--- Adding_Records.cpp
+++ Adding_Records.cpp
@@ -175,12 +175,13 @@ void Sort(date *&Date, int count){
 void Search(date *&Date, int count, int DD1, int MM1, int YYYY1, int DD2, int MM2, int YYYY2, int Type, double Amount1, double Amount2, string Info){
   int Flag = 0; //flag if there is a match
   cout << endl << "          Searching Result          " << endl;
+  cout <<  "Day " << setw(6) << "Month "  << "Year " <<  " I/E    " << " Type " << "Amount" << endl;
   for (int i = 0; i < count; i++)
     if (Date[i].Year <= YYYY2 && Date[i].Year >= YYYY1 && Date[i].Month <= MM2 && Date[i].Month >= MM1 && Date[i].Day <= DD2 && Date[i].Day >= DD1){ //if in the range
       if (Date[i].rec.Type == Type)
         if (Date[i].rec.Amount <= Amount2 && Date[i].rec.Amount >= Amount1)
           if (Date[i].rec.Info == Info){
-	          cout <<  "Day " << setw(6) << "Month "  << "Year " <<  " I/E    " << " Type " << "Amount" << endl;
+	          
             cout << Date[i].Day << "  " << Date[i].Month << "    " << Date[i].Year << "  ";
             if (Date[i].rec.Type == 2)
               cout << "Income" << "  ";
