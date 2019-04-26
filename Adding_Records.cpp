7eff01b0c07e9d0ee5fd2b2abfe484759dca4811
LI YIK WAI <47166479+liyikwai@users.noreply.github.com>
2019-04-26 19:43:51 +0800

    Update Adding_Records.cpp

--- Adding_Records.cpp
+++ Adding_Records.cpp
@@ -199,7 +199,7 @@ void Report(date *&Date, int count){ // produce an annual report
   double Income_E = 0, Income_F = 0, Income_P = 0;
   double Expense_T = 0, Expense_F = 0, Expense_L = 0;
   int Year = Date[0].Year;
-  cout << left;
+    cout<<left;
   cout << "Year : " << "Statistical Data" << "         Amount " << "Percentage " << endl; 
   for (int i = 0; i < count; i++){
     if (Date[i].Year == Year){
@@ -222,18 +222,30 @@ void Report(date *&Date, int count){ // produce an annual report
           Expense_L += Date[i].rec.Amount;
       }
     }
+        double P_Earned=Income_E * 100 / Annual_Income , P_Porfolio = Income_F * 100 / Annual_Income, P_Passive = Income_P * 100 / Annual_Income;
+          double P_Transportataion = Expense_T * 100 / Annual_Expense, P_Food = Expense_F * 100 / Annual_Expense, P_Living = Expense_L * 100 / Annual_Expense; 
+          if (Annual_Income == 0){
+            P_Earned = 0; 
+            P_Passive = 0; 
+            P_Passive = 0;
+          }
+          if (Annual_Expense == 0){
+            P_Transportataion = 0; 
+            P_Food = 0; 
+            P_Living = 0;
+          }
     if (Date[i].Year != Year || i == count - 1) { // if year changes or reaches the last one
-      cout << endl;
-      cout << left; 
+      cout<<endl;
+      cout<<left; 
       cout << Year << setw(28) << " : Average Monthly Income " << setw(5) << Annual_Income << "  \\ " << endl;
       cout << Year << setw(28) << " : Average Monthly Expense " << setw(5) << Annual_Expense << "  \\ " << endl;
       cout << Year << setw(28) << " : Net Income " << setw(5) << Annual_Income - Annual_Expense << "  \\ " << endl;
-      cout << Year << setw(28) << " : Earned Income " << setw(5) << Income_E << "  " << Income_E * 100 / Annual_Income << "%" << endl;
-      cout << Year << setw(28) << " : Porfolio Income " << setw(5) << Income_F << "  " << Income_F * 100 / Annual_Income << "%" << endl;
-      cout << Year << setw(28) << " : Passive Income " << setw(5) << Income_P << "  " << Income_P * 100 / Annual_Income << "%" << endl;
-      cout << Year << setw(28) << " : Transportation Expense " << setw(5) << Expense_T << "  " << Expense_T * 100 / Annual_Expense << "%" << endl;
-      cout << Year << setw(28) << " : Food & Drinks " << setw(5) << Expense_F << "  " << Expense_F * 100 / Annual_Expense << "%" << endl;
-      cout << Year << setw(28) << " : Living & Others " << setw(5) << Expense_L << "  " << Expense_L * 100 / Annual_Expense << "%" << endl;
+      cout << Year << setw(28) << " : Earned Income " << setw(5) << Income_E << "  " << P_Earned << "%" << endl;
+      cout << Year << setw(28) << " : Porfolio Income " << setw(5) << Income_F << "  " <<  P_Passive << "%" << endl;
+      cout << Year << setw(28) << " : Passive Income " << setw(5) << Income_P << "  " << P_Passive << "%" << endl;
+      cout << Year << setw(28) << " : Transportation Expense " << setw(5) << Expense_T << "  " << P_Transportataion << "%" << endl;
+      cout << Year << setw(28) << " : Food & Drinks " << setw(5) << Expense_F << "  " << P_Food << "%" << endl;
+      cout << Year << setw(28) << " : Living & Others " << setw(5) << Expense_L << "  " << P_Living << "%" << endl;
       Year = Date[i].Year; // year changes
       Annual_Income = Annual_Expense = 0;
       Income_E = Income_F = Income_P = 0;
@@ -247,6 +259,7 @@ void Report(date *&Date, int count){ // produce an annual report
     }
   }
 }
+
 
 void Calculate_Monthly(date * &Date, int count, double &Monthly_Balance, double &Monthly_Income, double &Monthly_Expense){
   double Months = (Date[count - 1].Year - Date[0].Year) * 12 + (Date[count - 1].Month - Date[0].Month) + 1; // how many months there are
@@ -396,6 +409,8 @@ int main() {
     fout << "          Financial Record           " << endl;
     fout <<  "Day " << setw(6) << "Month "  << "Year " <<  " I/E    " << " Type " << "Amount" << endl; 
     fout<<left;
+    total_income = 0;
+    total_expense = 0;
     for (int i = 0; i < count; i++){
       if (Date[i].rec.Type == 0)
         break;
