660927cddf8c8601dbb612a7bed7f38b291d4b56
LI YIK WAI <47166479+liyikwai@users.noreply.github.com>
2019-04-27 19:01:18 +0800

    V2.0.3

--- DeleteChange.h
+++ DeleteChange.h
@@ -2,6 +2,6 @@
 #include <string>
 #ifndef __DELETECHANGE_H__
 #define __DELETECHANGE_H__
-void Delete(date *&Date, int &size, int DD, int MM, int YYYY, int Del_Type, int Number, std::string Info);
+void Delete(date *&Date, int &size, int &count, int DD, int MM, int YYYY, int Del_Type, int Number, std::string Info);
 void Change(date *&Date, int size, int DD, int MM, int YYYY, int Change_Type, int Number, std::string Info);
 #endif
